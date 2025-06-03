87e17150a050ab35562dddfbeda2066895c5355e
3lson <139233586+3lson@users.noreply.github.com>
2025-06-03 22:45:44 +0100

    Revert data_mem and clean_up - we will be restarting the implementation taking a new approach - BRAM controller

--- rtl/data_mem.sv
+++ rtl/data_mem.sv
@@ -1,43 +1,45 @@
 module data_mem #(
-    parameter DATA_WIDTH       = 32,
-              ADDR_WIDTH       = 32,
-              ADDR_REAL_WIDTH  = 15,  // 2^15 = 32K words = 128KB total
-              MEM_WIDTH        = 32
+    parameter DATA_WIDTH = 32, 
+              ADDR_WIDTH = 32,
+              ADDR_REAL_WIDTH = 20, // 2^20 = 1MB memory
+              MEM_WIDTH = 8
 )(
     input  logic                    clk,
     input  logic                    WDME,
-    input  logic [ADDR_WIDTH-1:0]   A,        // CPU address
-    input  logic [DATA_WIDTH-1:0]   WD,       // Write data
-    output logic [DATA_WIDTH-1:0]   RD,       // Read data
-
-    // AXI I/O access (read-only for now)
-    input  logic [ADDR_WIDTH-1:0]   axi_A,
-    output logic [DATA_WIDTH-1:0]   axi_RD
+    input  logic [ADDR_WIDTH-1:0]   A, // full address
+    input  logic [DATA_WIDTH-1:0]   WD, // write data
+    output logic [DATA_WIDTH-1:0]   RD  // read data
 );
 
-    // Use BRAM explicitly
-    (* ram_style = "block" *) logic [MEM_WIDTH-1:0] array [0:(1 << ADDR_REAL_WIDTH) - 1];
+    logic [31:0] addr;
+    logic [MEM_WIDTH-1:0] array [2**ADDR_REAL_WIDTH-1:0];
+    logic [DATA_WIDTH-1:0] temp;
 
-    logic [ADDR_REAL_WIDTH-1:0] word_addr, axi_word_addr;
-
-    assign word_addr     = A[ADDR_REAL_WIDTH+1:2];     // word-aligned
-    assign axi_word_addr = axi_A[ADDR_REAL_WIDTH+1:2]; // word-aligned
+    assign addr = $unsigned(A);
 
     initial begin
-        $display("Loading program into data memory...");
         for (int i = 0; i < (1 << ADDR_REAL_WIDTH); i++) begin
-            array[i] = 32'h00000000;
+            array[i] = 8'b0;
         end
-        $readmemh("../rtl/data.hex", array);
+        $display("Loading program into data memory...");
+        $readmemh("../rtl/data.hex", array);  
     end
 
-    // Synchronous read and write
+    // Load instruction
+    always_comb begin
+        temp = {array[addr+3], array[addr+2], array[addr+1], array[addr]};
+    end
+
+    // Store instruction
     always_ff @(posedge clk) begin
         if (WDME) begin
-            array[word_addr] <= WD;
+            array[addr]     <= WD[7:0];
+            array[addr + 1] <= WD[15:8];
+            array[addr + 2] <= WD[23:16];
+            array[addr + 3] <= WD[31:24];
         end
-        RD      <= array[word_addr];
-        axi_RD  <= array[axi_word_addr];
     end
+
+    assign RD = temp;
 
 endmodule
